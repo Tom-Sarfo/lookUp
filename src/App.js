f345a442df304d552d924f9dbcec6239a19ca506
Thomas Sarfo <tomsarfodavis@gmail.com>
2023-08-11 08:22:36 +0000

    add third-party package files MUI-roboto-fonts-mui-icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import logo from './logo.svg';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './App.css';
 
 function App() {

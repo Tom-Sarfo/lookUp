a8ae7e8671b371b13e2a8bc0822280ae9e2b0f29
Thomas Sarfo <tomsarfodavis@gmail.com>
2023-08-11 07:51:33 +0000

    add test drug data

---
+++ data.js
@@ -0,0 +1,44 @@
+export const drug = [
+	{
+		drugID: 1,
+		drugName: "Lufart DS tab x 1",
+		availability: 50,
+		price: 20,
+		alternative: [{ drugID: 3 }],
+	},
+	{
+		drugID: 2,
+		drugName: "Amoksiklav 1g tablets x 10",
+		availability: 50,
+		price: 20,
+		alternative: [{ drugID: 6 }],
+	},
+	{
+		drugID: 3,
+		drugName: "Lonart Forte tab x 1",
+		availability: 50,
+		price: 20,
+		alternative: [{ drugID: 1 }],
+	},
+	{
+		drugID: 4,
+		drugName: "Para-Denk 250mg Suppositories x10",
+		availability: 50,
+		price: 20,
+		alternative: [{ drugID: 5 }],
+	},
+	{
+		drugID: 5,
+		drugName: "Gebemol-250mg suppositories x 10",
+		availability: 50,
+		price: 20,
+		alternative: [{ drugID: 4 }],
+	},
+	{
+		drugID: 6,
+		drugName: "Augmentin 1g tablets x 14",
+		availability: 50,
+		price: 20,
+		alternative: [{ drugID: 2 }],
+	},
+];
